a42221bbac0d022a7c64387af0b72588005a7022
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-09-08 17:07:59 -0300

    mocks

---
+++ src/data/data-buttons.tsx
@@ -0,0 +1,16 @@
+import type { MenuLinkProps } from "../components/navbar/MenuLink";
+
+export const linkButtons: MenuLinkProps[] = [
+  {
+    text: "Início",
+    to: "/home",
+  },
+  {
+    text: "Jogos",
+    to: "/games",
+  },
+  {
+    text: "Minha Lista",
+    to: "/my-games",
+  },
+];
---
+++ src/data/mock/carousel-mock.ts
@@ -0,0 +1,27 @@
+export const carouselMock = [
+  {
+    src: "mock/red-dead.png",
+    title: "Read Dead 2",
+    to: "/games/2",
+    video: "https://youtu.be/gmA6MrX81z4",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
+  },
+  {
+    src: "mock/silk-song.png",
+    title: "SilkSong",
+    to: "/games/1",
+    video: "https://youtu.be/6XGeJwsUP9c",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
+  },
+  {
+    src: "mock/spiderman.png",
+    title: "Marvel's SpiderMan Remastered",
+    to: "/games/3",
+    video:
+      "https://www.youtube.com/watch?v=mrT5q5xXb7Y&ab_channel=MarvelEntertainment",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
+  },
+];
