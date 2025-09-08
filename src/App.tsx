4f56ebfce3d0c9c5191499f32f1a3ad1c435078c
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-09-08 20:38:15 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import { AppContextProvider } from "./contexts/AppContext";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <AppContextProvider>
       <BrowserRouter>
-        <Home />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
       </BrowserRouter>
     </AppContextProvider>
   );
