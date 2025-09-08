6b8c584c08fa86d759c59c65a11c4df9e5f4e0ec
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-09-08 20:42:08 -0300

    implementing slider logic

--- src/components/games/Slider.tsx
+++ src/components/games/Slider.tsx
@@ -1,6 +1,8 @@
 import GameCardList from "./GameCardList";
 import type { GameCardProps } from "./GameCard";
 import SliderControls from "./SliderControls";
+import useSlider from "../../hooks/useSlider";
+import React from "react";
 
 type SliderProps = {
   title: string;
@@ -8,16 +10,38 @@ type SliderProps = {
 };
 
 function Slider({ title, data }: SliderProps) {
+  const ref = React.useRef<HTMLDivElement>(null);
+  const {
+    handlePrev,
+    handleNext,
+    handleMouseDown,
+    handleMouseMove,
+    handleMouseUp,
+    canScrollToRight,
+    canScrollToLeft,
+  } = useSlider(ref as React.RefObject<HTMLDivElement>);
+
   return (
-    <div className="flex flex-col px-6 flex-nowrap gap-4 w-full mx-auto  mt-10">
+    <div className="flex flex-col  flex-nowrap gap-4 w-full   mt-10">
       <div className="flex items-center justify-between">
         <h2 className="text-2xl text-stone-950 dark:text-stone-100  ">
           {title}
         </h2>
 
-        <SliderControls handleNext={() => {}} handlePrev={() => {}} />
+        <SliderControls
+          canScrollToLeft={canScrollToLeft}
+          canScrollToRight={canScrollToRight}
+          handleNext={handleNext}
+          handlePrev={handlePrev}
+        />
       </div>
-      <div className=" overflow-x-scroll scrollbar-hide">
+      <div
+        ref={ref}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+        className="scroll-smooth snap-x overflow-x-scroll scrollbar-hide "
+      >
         <GameCardList data={data} />
       </div>
     </div>
@@ -25,3 +49,4 @@ function Slider({ title, data }: SliderProps) {
 }
 
 export default Slider;
+// scrollbar-hide
---
+++ src/hooks/useSlider.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect } from "react";
+
+function useSlider(ref: React.RefObject<HTMLDivElement>) {
+  const scrollAmount = 343;
+  const [canScrollToRight, setCanScrollToRight] = React.useState(true);
+  const [canScrollToLeft, setCanScrollToLeft] = React.useState(false);
+
+  const [isDragging, setIsDragging] = React.useState(false);
+  const [startX, setStartX] = React.useState(0);
+
+  const handleMouseDown = (e: React.MouseEvent) => {
+    setIsDragging(true);
+    setStartX(e.clientX);
+  };
+  const handleMouseMove = (e: React.MouseEvent) => {
+    if (!isDragging) return;
+    let diff = e.clientX - startX;
+
+    console.log(diff);
+  };
+
+  const handleMouseUp = (e: React.MouseEvent) => {
+    if (!isDragging) return;
+    const diff = e.clientX - startX;
+
+    if (diff < 0) handleNext();
+    else handlePrev();
+    setIsDragging(false);
+  };
+
+  const updateScrollState = (scrollLeft?: number) => {
+    if (!ref.current) return;
+    const currentScroll = scrollLeft ?? ref.current.scrollLeft;
+    const { scrollWidth, clientWidth } = ref.current;
+
+    setCanScrollToLeft(currentScroll > 0);
+    setCanScrollToRight(currentScroll + clientWidth < scrollWidth);
+  };
+
+  const handlePrev = () => {
+    if (!ref.current) return;
+    const newScroll = Math.max(ref.current.scrollLeft - scrollAmount, 0);
+    ref.current.scrollLeft = newScroll;
+    updateScrollState(newScroll);
+  };
+
+  const handleNext = () => {
+    if (!ref.current) return;
+    const maxScrollLeft = ref.current.scrollWidth - ref.current.clientWidth;
+    const newScroll = Math.min(
+      ref.current.scrollLeft + scrollAmount,
+      maxScrollLeft
+    );
+    ref.current.scrollLeft = newScroll;
+    updateScrollState(newScroll);
+  };
+
+  useEffect(() => {
+    updateScrollState();
+  }, [ref.current]);
+
+  return {
+    handlePrev,
+    handleNext,
+    handleMouseDown,
+    handleMouseMove,
+    handleMouseUp,
+    canScrollToRight,
+    canScrollToLeft,
+  };
+}
+
+export default useSlider;
