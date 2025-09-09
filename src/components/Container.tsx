ee8608d8a67e7e930385bb78b1f36a24ba5d45fd
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-09-09 10:42:54 -0300

    Create Container.tsx

---
+++ src/components/Container.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Container({ children }: { children: React.ReactNode }) {
+  return <div className="mx-auto px-6">{children}</div>;
+}
+
+export default Container;
