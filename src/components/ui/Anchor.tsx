864049b00eddf1a37f07f9a5c7b1531fd92eaf3d
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-09-09 10:40:57 -0300

    Create Anchor.tsx

---
+++ src/components/ui/Anchor.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function Anchor({ children, to }: { children: React.ReactNode; to: string }) {
+  const navigate = useNavigate();
+  return (
+    <a
+      onClick={() => navigate(to)}
+      className="text-stone-950 text-xs underline-offset-2 underline dark:text-stone-400"
+    >
+      {children}
+    </a>
+  );
+}
+
+export default Anchor;
