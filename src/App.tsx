10261133089ac591d8a222593011dba7b779f464
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-09-13 16:02:01 -0300

    new routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,32 @@ import Home from "./pages/Home";
 import { AppContextProvider } from "./contexts/AppContext";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import Catalog from "./pages/Catalog";
+import { SidebarProvider } from "./contexts/SidebarContext";
+import { QueryProvider } from "./contexts/QueryContext";
+import Game from "./pages/Game";
 
 function App() {
   return (
     <AppContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/" element={<Login />} />
-          <Route path="/sign-up" element={<SignUp />} />
-        </Routes>
-      </BrowserRouter>
+      <SidebarProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route
+              path="/home/games"
+              element={
+                <QueryProvider>
+                  <Catalog />
+                </QueryProvider>
+              }
+            />
+            <Route path="/home/game/:id" element={<Game />} />
+          </Routes>
+        </BrowserRouter>
+      </SidebarProvider>
     </AppContextProvider>
   );
 }
