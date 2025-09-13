04b0208fe31ddc80f868d34a9650e70249f9a54f
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-09-13 18:58:00 -0300

    Update Mainlayout.tsx

--- src/layout/Mainlayout.tsx
+++ src/layout/Mainlayout.tsx
@@ -1,9 +1,9 @@
 import React, { useContext } from "react";
 import Navbar from "../components/navbar/Navbar";
 import { AppContext } from "../contexts/AppContext";
-import PlayerModal from "../components/player/PlayerModal";
 import Modal from "../components/Modal";
 import { AnimatePresence } from "framer-motion";
+import PlayerModal from "../components/ui/PlayerModal";
 
 function Mainlayout({ children }: { children: React.ReactNode }) {
   const { isModalOpen, currentVideo } = useContext(AppContext);
