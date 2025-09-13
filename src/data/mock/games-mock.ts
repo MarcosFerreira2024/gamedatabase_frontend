4da1c2179fc3fb934b165330dcf09ba546e12963
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-09-13 16:10:57 -0300

    updating mocks

--- src/data/mock/games-mock.ts
+++ src/data/mock/games-mock.ts
@@ -6,8 +6,8 @@ export const gameCardMock: GameCardProps[] = [
     favorited: false,
     video:
       "https://www.youtube.com/watch?v=mrT5q5xXb7Y&ab_channel=MarvelEntertainment",
-    image: "mock/spiderman.png",
-    link: "/games/3",
+    image: "/mock/spiderman.png",
+    link: "/home/game/3",
     id: "3",
     name: "Marvel's SpiderMan Remastered",
     platform: "Playstation 5",
@@ -15,8 +15,8 @@ export const gameCardMock: GameCardProps[] = [
   {
     favorited: false,
     video: "https://www.youtube.com/watch?v=gmA6MrX81z4",
-    image: "mock/red-dead.png",
-    link: "/games/2",
+    image: "/mock/red-dead.png",
+    link: "/home/game/2",
     id: "2",
     name: "Red Dead Redemption 2",
     platform: "Playstation 5",
@@ -24,8 +24,8 @@ export const gameCardMock: GameCardProps[] = [
   {
     favorited: false,
     video: "https://www.youtube.com/watch?v=6XGeJwsUP9c",
-    image: "mock/silk-song.png",
-    link: "/games/1",
+    image: "/mock/silk-song.png",
+    link: "/home/game/1",
     id: "1",
     name: "SilkSong",
     platform: "Playstation 5",
@@ -34,8 +34,8 @@ export const gameCardMock: GameCardProps[] = [
     favorited: false,
     video:
       "https://www.youtube.com/watch?v=mrT5q5xXb7Y&ab_channel=MarvelEntertainment",
-    image: "mock/spiderman.png",
-    link: "/games/3",
+    image: "/mock/spiderman.png",
+    link: "/home/game/3",
     id: "3",
     name: "Marvel's SpiderMan Remastered",
     platform: "Playstation 5",
@@ -43,8 +43,8 @@ export const gameCardMock: GameCardProps[] = [
   {
     favorited: false,
     video: "https://www.youtube.com/watch?v=gmA6MrX81z4",
-    image: "mock/red-dead.png",
-    link: "/games/2",
+    image: "/mock/red-dead.png",
+    link: "/home/game/2",
     id: "2",
     name: "Red Dead Redemption 2",
     platform: "Playstation 5",
@@ -52,8 +52,62 @@ export const gameCardMock: GameCardProps[] = [
   {
     favorited: false,
     video: "https://www.youtube.com/watch?v=6XGeJwsUP9c",
-    image: "mock/silk-song.png",
-    link: "/games/1",
+    image: "/mock/silk-song.png",
+    link: "/home/game/1",
+    id: "1",
+    name: "SilkSong",
+    platform: "Playstation 5",
+  },
+  {
+    favorited: false,
+    video: "https://www.youtube.com/watch?v=6XGeJwsUP9c",
+    image: "/mock/silk-song.png",
+    link: "/home/game/1",
+    id: "1",
+    name: "SilkSong",
+    platform: "Playstation 5",
+  },
+  {
+    favorited: false,
+    video: "https://www.youtube.com/watch?v=6XGeJwsUP9c",
+    image: "/mock/silk-song.png",
+    link: "/home/game/1",
+    id: "1",
+    name: "SilkSong",
+    platform: "Playstation 5",
+  },
+  {
+    favorited: false,
+    video: "https://www.youtube.com/watch?v=6XGeJwsUP9c",
+    image: "/mock/silk-song.png",
+    link: "/home/game/1",
+    id: "1",
+    name: "SilkSong",
+    platform: "Playstation 5",
+  },
+  {
+    favorited: false,
+    video: "https://www.youtube.com/watch?v=6XGeJwsUP9c",
+    image: "/mock/silk-song.png",
+    link: "/home/game/1",
+    id: "1",
+    name: "SilkSong",
+    platform: "Playstation 5",
+  },
+  {
+    favorited: false,
+    video: "https://www.youtube.com/watch?v=6XGeJwsUP9c",
+    image: "/mock/silk-song.png",
+    link: "/home/game/1",
+    id: "1",
+    name: "SilkSong",
+    platform: "Playstation 5",
+  },
+  {
+    favorited: false,
+    video: "https://www.youtube.com/watch?v=6XGeJwsUP9c",
+    image: "/mock/silk-song.png",
+    link: "/home/game/1",
     id: "1",
     name: "SilkSong",
     platform: "Playstation 5",
@@ -94,3 +148,31 @@ export const upcomingGameMock: UpcomingCardProps[] = [
     image: "mock/silk-song.png",
   },
 ];
+
+export const gameGaleryMock = {
+  name: "Marvel's SpiderMan Remastered",
+  rating: "8.5",
+  video: "https://www.youtube.com/watch?v=mrT5q5xXb7Y",
+  summary:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+  images: [
+    {
+      image: "/mock/spiderman.png",
+    },
+    {
+      image: "/mock/silk-song.png",
+    },
+    {
+      image: "/mock/red-dead.png",
+    },
+    {
+      image: "/mock/red-dead.png",
+    },
+    {
+      image: "/mock/red-dead.png",
+    },
+    {
+      image: "/mock/red-dead.png",
+    },
+  ],
+};
